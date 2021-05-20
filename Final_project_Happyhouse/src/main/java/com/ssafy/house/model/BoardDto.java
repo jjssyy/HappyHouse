78ed1b49e4fcf81797fa3d7efbc11641201cb23c
Si yeong Jeon <nnaow88@gmail.com>
2021-05-20 14:37:13 +0900

    게시판dto

---
+++ Final_project_Happyhouse/src/main/java/com/ssafy/house/model/BoardDto.java
@@ -0,0 +1,34 @@
+package com.ssafy.house.model;
+
+public class BoardDto {
+	private int no;
+	private int post_no;
+	private String name;
+	private String content;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getPost_no() {
+		return post_no;
+	}
+	public void setPost_no(int post_no) {
+		this.post_no = post_no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
