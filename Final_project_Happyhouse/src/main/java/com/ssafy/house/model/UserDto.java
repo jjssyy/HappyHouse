7d2254aeb0aefd39cf2adb4571639bac235e0332
Si yeong Jeon <nnaow88@gmail.com>
2021-05-20 14:32:10 +0900

    userdto

---
+++ Final_project_Happyhouse/src/main/java/com/ssafy/house/model/UserDto.java
@@ -0,0 +1,55 @@
+package com.ssafy.house.model;
+
+public class UserDto {
+	private String userid;
+	private String userpwd;
+	private String username;
+	private String email;
+	private String interest_sido;
+	private String interest_gugun;
+	private String interest_dong;
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUserpwd() {
+		return userpwd;
+	}
+	public void setUserpwd(String userpwd) {
+		this.userpwd = userpwd;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getInterest_sido() {
+		return interest_sido;
+	}
+	public void setInterest_sido(String interest_sido) {
+		this.interest_sido = interest_sido;
+	}
+	public String getInterest_gugun() {
+		return interest_gugun;
+	}
+	public void setInterest_gugun(String interest_gugun) {
+		this.interest_gugun = interest_gugun;
+	}
+	public String getInterest_dong() {
+		return interest_dong;
+	}
+	public void setInterest_dong(String interest_dong) {
+		this.interest_dong = interest_dong;
+	}
+	
+}
