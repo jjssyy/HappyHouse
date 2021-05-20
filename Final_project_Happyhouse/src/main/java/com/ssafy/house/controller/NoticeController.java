e115374a75128f7a28e1a4bbe6c8dd49a1406cd3
Si yeong Jeon <nnaow88@gmail.com>
2021-05-20 23:52:06 +0900

    공지사항 controller

---
+++ Final_project_Happyhouse/src/main/java/com/ssafy/house/controller/NoticeController.java
@@ -0,0 +1,62 @@
+package com.ssafy.house.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.house.model.NoticeBoardDto;
+import com.ssafy.house.model.service.NoticeBoardService;
+
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@RestController
+@RequestMapping("/notice")
+public class NoticeController {
+	
+	@Autowired
+	NoticeBoardService noticeBoardService;
+	
+	@GetMapping
+	public List<NoticeBoardDto> noticeList(){
+		return noticeBoardService.noticeList();
+	}
+	
+	@GetMapping("/{no}")
+	public NoticeBoardDto getNotice(@PathVariable int no) {
+		return noticeBoardService.getNotice(no);
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> writeNotice(@RequestBody NoticeBoardDto noticeBoardDto){
+		if(noticeBoardService.writeNotice(noticeBoardDto)==1) {
+			return new ResponseEntity<String>("success",HttpStatus.OK);
+		}
+		return new ResponseEntity<String>("Fail",HttpStatus.NO_CONTENT);
+	}
+	
+	@PutMapping("{no}")
+	public ResponseEntity<String> modifyNotice(@RequestBody NoticeBoardDto noticeBoardDto){
+		if(noticeBoardService.modifyNotice(noticeBoardDto)==1) {
+			return new ResponseEntity<String>("success",HttpStatus.OK);
+		}
+		return new ResponseEntity<String>("Fail",HttpStatus.NO_CONTENT);
+	}
+	
+	@DeleteMapping("{no}")
+	public ResponseEntity<String> deleteNotice(@PathVariable int no) {
+		if(noticeBoardService.deleteNotice(no)==1) {
+			return new ResponseEntity<String>("success",HttpStatus.OK);
+		}
+		return new ResponseEntity<String>("Fail",HttpStatus.NO_CONTENT);
+	}
+}
