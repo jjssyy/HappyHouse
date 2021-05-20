f4754235029e8033cb571c47960a241b66111e04
Si yeong Jeon <nnaow88@gmail.com>
2021-05-20 21:07:17 +0900

    .

--- Final_project_Happyhouse/src/main/java/com/ssafy/house/model/service/BoardServiceImpl.java
+++ Final_project_Happyhouse/src/main/java/com/ssafy/house/model/service/BoardServiceImpl.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ssafy.house.model.BoardDto;
 import com.ssafy.house.model.mapper.BoardMapper;
 
+@Service
 public class BoardServiceImpl implements BoardService {
 	private static final Logger logger = LoggerFactory.getLogger(BoardServiceImpl.class);
 	
