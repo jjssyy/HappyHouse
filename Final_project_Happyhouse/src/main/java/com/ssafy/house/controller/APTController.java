0ec95b051f1fdfd50a46c0b87027a8254c84c79d
MojinHyeok <76107714+MojinHyeok@users.noreply.github.com>
2021-05-21 16:10:27 +0900

    commit

--- Final_project_Happyhouse/src/main/java/com/ssafy/house/controller/APTController.java
+++ Final_project_Happyhouse/src/main/java/com/ssafy/house/controller/APTController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +20,7 @@ import com.ssafy.house.model.service.GugunService;
 import com.ssafy.house.model.service.SidoService;
 
 @RestController
+@CrossOrigin("*")
 public class APTController {
 	
 	@Autowired
@@ -46,9 +48,24 @@ public class APTController {
 	@RequestMapping(value = "/gugun", method = RequestMethod.POST, headers = { "Content-type=application/json" })
 	public List<GugunDto> GugunList(@RequestBody Map<String,String> map) {
 		String sido=map.get("sido_code");
+		System.out.println(sido);
 		sido=sido.substring(0,2);
 		return gugun.getListGugun(sido);
 	}
+	
+	//test
+//	@RequestMapping(value = "/gugun", method = RequestMethod.POST, headers = { "Content-type=application/json" })
+//	public List<GugunDto> GugunList(@RequestBody String sido_code) {
+//		String sido=sido_code;
+//		System.out.println(sido);
+//		sido=sido.substring(0,2);
+//		return gugun.getListGugun(sido);
+//	}
+//	
+	
+	
+	
+	
 	@RequestMapping(value = "/dong", method = RequestMethod.POST, headers = { "Content-type=application/json" })
 	public List<DongDto> dongList(@RequestBody Map<String,String> map) {
 		String gugun=map.get("gugun_code");
