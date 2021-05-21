052c66e49f650e017e263cb713105dcb1b78c7b5
Si yeong Jeon <nnaow88@gmail.com>
2021-05-22 00:33:16 +0900

    로그인, 회원정보, 회원가입

--- Final_project_Happyhouse/src/main/java/com/ssafy/house/controller/UserController.java
+++ Final_project_Happyhouse/src/main/java/com/ssafy/house/controller/UserController.java
@@ -1,20 +1,92 @@
 package com.ssafy.house.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.ssafy.house.model.service.JwtService;
+import com.ssafy.house.model.UserDto;
+import com.ssafy.house.model.service.JwtServiceImpl;
+import com.ssafy.house.model.service.UserService;
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	public static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
 	
 	@Autowired
-	private JwtService jwtService;
+	private JwtServiceImpl jwtService;
 	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/confirm/login")
+	public ResponseEntity<Map<String, Object>> login(@RequestBody UserDto userDto) {
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
+		try {
+			UserDto loginUser = userService.login(userDto);
+			if (loginUser != null) {
+				String token = jwtService.create("userid", loginUser.getUserid(), "access-token");// key, data, subject
+				logger.debug("로그인 토큰정보 : {}", token);
+				resultMap.put("access-token", token);
+				resultMap.put("message", SUCCESS);
+				status = HttpStatus.ACCEPTED;
+			} else {
+				resultMap.put("message", FAIL);
+				status = HttpStatus.ACCEPTED;
+			}
+		} catch (Exception e) {
+			logger.error("로그인 실패 : {}", e);
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+	
+	@GetMapping("/info/{userid}")
+	public ResponseEntity<Map<String, Object>> userInfo(
+			@PathVariable("userid") String userid, HttpServletRequest request) {
+//		logger.debug("userid : {} ", userid);
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		if (jwtService.isUsable(request.getHeader("access-token"))) {
+			logger.info("사용 가능한 토큰!!!");
+			try {
+//				로그인 사용자 정보.
+				UserDto userDto = userService.userInfo(userid);
+				resultMap.put("userInfo", userDto);
+				resultMap.put("message", SUCCESS);
+				status = HttpStatus.ACCEPTED;
+			} catch (Exception e) {
+				logger.error("정보조회 실패 : {}", e);
+				resultMap.put("message", e.getMessage());
+				status = HttpStatus.INTERNAL_SERVER_ERROR;
+			}
+		} else {
+			logger.error("사용 불가능 토큰!!!");
+			resultMap.put("message", FAIL);
+			status = HttpStatus.ACCEPTED;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+	
+	@PostMapping("/register")
+	public ResponseEntity<String> userRegister(@RequestBody UserDto userDto){
+		if(userService.userRegister(userDto)==1) {
+			return new ResponseEntity<String>("success",HttpStatus.OK);
+		}
+		return new ResponseEntity<String>("Fail",HttpStatus.NO_CONTENT);
+	}
 	
 }
