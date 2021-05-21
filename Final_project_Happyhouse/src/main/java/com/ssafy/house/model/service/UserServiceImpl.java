f17e688bfae081350751e0136efb8e52ff693aa3
Si yeong Jeon <nnaow88@gmail.com>
2021-05-22 01:53:30 +0900

    @service 추가

--- Final_project_Happyhouse/src/main/java/com/ssafy/house/model/service/UserServiceImpl.java
+++ Final_project_Happyhouse/src/main/java/com/ssafy/house/model/service/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.ssafy.house.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ssafy.house.model.UserDto;
 import com.ssafy.house.model.mapper.UserMapper;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
