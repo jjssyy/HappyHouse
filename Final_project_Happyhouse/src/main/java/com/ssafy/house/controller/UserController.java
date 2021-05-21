a9f946ee3fa1ea5a3e467e2428b49a6944130726
Si yeong Jeon <nnaow88@gmail.com>
2021-05-21 13:21:52 +0900

    dependency추가 jwt

--- Final_project_Happyhouse/src/main/java/com/ssafy/house/controller/UserController.java
+++ Final_project_Happyhouse/src/main/java/com/ssafy/house/controller/UserController.java
@@ -1,5 +1,14 @@
 package com.ssafy.house.controller;
 
-public class UserController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	public static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	
 }
